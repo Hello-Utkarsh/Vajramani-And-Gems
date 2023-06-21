d3ebbd21b5929b6735587bc53e9a5d36feded9a1
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-06-21 20:46:07 +0530

    added dropdowns

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='bg-black text-[#C07F00] w-[100vw] border-2 h-[50vh]'>
-      <div className='flex justify-between p-6 '>
+    <div className='bg-black text-[#C07F00] w-[100vw]'>
+      <div className='flex justify-between p-6 flex-wrap'>
 
         {/* contacts */}
 
@@ -36,13 +36,13 @@ const Contact = () => {
 
       {/* social accounts */}
 
-      <div className='w-[80%] mt-8 m-auto flex justify-around'>
+      <div className='w-[80%] mt-8 m-auto flex justify-around flex-wrap'>
         <h4 className='text-lg'>Also available on</h4>
-        <div className='flex w-[50%] justify-around'>
-        <img className='rounded-lg bg-[#C07F00] h-10 transition-all ease-in-out  hover:bg-gradient-to-b from-[#405DE6] from-10% via-[#833AB4] via-75% to-[#C13584] to-100% duration-500 hover:-translate-y-2 hover:scale-125' src={"src/assets/instagram.png"} alt="nstagram logo icons created by Hight Quality Icons - Flaticon" />
-        <img className='rounded-lg bg-[#C07F00] h-10 transition-all ease-in-out  hover:bg-gradient-to-b from-[#405DE6] from-10% via-[#833AB4] via-75% to-[#C13584] to-100% duration-500 hover:-translate-y-2 hover:scale-125' src={"src/assets/instagram.png"} alt="nstagram logo icons created by Hight Quality Icons - Flaticon" />
-        <img className='transition ease-in-out duration-500 hover:-translate-y-2 hover:scale-125 h-10 hover:bg-blue-500 rounded-lg bg-[#C07F00]' src={"src/assets/facebook.png"} alt="" />
-        <img className='transition ease-in-out duration-500 hover:-translate-y-2 hover:scale-125 h-10 hover:bg-blue-500 rounded-lg bg-[#C07F00]' src={"src/assets/facebook.png"} alt="" />
+        <div className='flex w-[50%] justify-around border-2 mx-8'>
+        <img className='mx-2 rounded-lg bg-[#C07F00] h-10 transition-all ease-in-out  hover:bg-gradient-to-b from-[#405DE6] from-10% via-[#833AB4] via-75% to-[#C13584] to-100% duration-500 hover:-translate-y-2 hover:scale-125' src={"src/assets/instagram.png"} alt="nstagram logo icons created by Hight Quality Icons - Flaticon" />
+        <img className='mx-2 rounded-lg bg-[#C07F00] h-10 transition-all ease-in-out  hover:bg-gradient-to-b from-[#405DE6] from-10% via-[#833AB4] via-75% to-[#C13584] to-100% duration-500 hover:-translate-y-2 hover:scale-125' src={"src/assets/instagram.png"} alt="nstagram logo icons created by Hight Quality Icons - Flaticon" />
+        <img className='mx-2 transition ease-in-out duration-500 hover:-translate-y-2 hover:scale-125 h-10 hover:bg-blue-500 rounded-lg bg-[#C07F00]' src={"src/assets/facebook.png"} alt="" />
+        <img className='mx-2 transition ease-in-out duration-500 hover:-translate-y-2 hover:scale-125 h-10 hover:bg-blue-500 rounded-lg bg-[#C07F00]' src={"src/assets/facebook.png"} alt="" />
         </div>
 
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 const Navbar = () => {
 
+    // DROPDOWN LOGIC
+    const dropdown = async () => {
+        let target_dropdown = Array.from(event.target.children).forEach(element => {
+            if (element.style.display == "" || element.style.display == "none") {
+                element.style.display = "grid"
+            }
+            else {
+                element.style.display = "none"
+            }
+
+        })
+
+    }
+
+    // CLOSE DROPDOWN IF CLICKED ANYWHERE ELSE
+    window.onclick = async (event) => {
+        if (!event.target.matches('.hover-dropdown')) {
+            Array.from(document.getElementsByClassName("dropdown-content")).forEach(element=>{
+                if (element.style.display == "grid") {
+                    console.log(element)
+                    element.style.display = "none"
+                }
+            })
+        }
+    }
+
+
     return (
         <div className='bg-[#FFD95A]'>
             <div className='flex w-[100vw] bg-[#C07F00] py-6 items-center'>
@@ -13,20 +40,72 @@ const Navbar = () => {
             <div className='flex w-[100vw] bg-[#C07F00] text-[#4C3D3D] item-center justify-center text-lg px-6 pb-3'>
 
                 <nav className='flex list-none w-[83%] justify-between h-12 items-center'>
-                    <ul className='cursor-pointer hover-dropdown'>
+                    <ul className='cursor-pointer hover-dropdown' onClick={dropdown}>
                         Calcite
+                        <div className='rounded-xl hidden grid-cols-3 justify-around px-2 text-[#4C3D3D] bg-[#FFD95A] -ml-16 mt-3 absolute w-[28vh] dropdown-content'>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                        </div>
                     </ul>
-                    <li className='hidden'>hello</li>
-                    <ul className='cursor-pointer hover-dropdown'>Chalcedony</ul>
-                    <li className='hidden'>hello</li>
-                    <ul className='cursor-pointer hover-dropdown'>Corundum</ul>
-                    <li className='hidden'>hello</li>
-                    <ul className='cursor-pointer hover-dropdown'>Quartz</ul>
-                    <li className='hidden'>hello</li>
-                    <ul className='cursor-pointer hover-dropdown'>Tourmaline</ul>
-                    <li className='hidden'>hello</li>
-                    <ul className='cursor-pointer hover-dropdown'>More</ul>
-                    <li className='hidden'>hello</li>
+                    <ul className='cursor-pointer hover-dropdown flex-col' onClick={dropdown}>
+                        Chalcedony
+                        <div className='rounded-xl hidden grid-cols-3 justify-around px-2 text-[#4C3D3D] bg-[#FFD95A] -ml-16 mt-3 absolute w-[28vh] dropdown-content'>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                        </div>
+                    </ul>
+                    <ul className='cursor-pointer hover-dropdown' onClick={dropdown}>
+                        Corundum
+                        <div className='rounded-xl hidden grid-cols-3 justify-around px-2 text-[#4C3D3D] bg-[#FFD95A] -ml-16 mt-3 absolute w-[28vh] dropdown-content'>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                        </div>
+                    </ul>
+                    <ul className='cursor-pointer hover-dropdown' onClick={dropdown}>
+                        Quartz
+                        <div className='rounded-xl hidden grid-cols-3 justify-around px-2 text-[#4C3D3D] bg-[#FFD95A] -ml-16 mt-3 absolute w-[28vh] dropdown-content'>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                        </div>
+                    </ul>
+                    <ul className='cursor-pointer hover-dropdown' onClick={dropdown}>
+                        Tourmaline
+                        <div className='rounded-xl hidden grid-cols-3 justify-around px-2 text-[#4C3D3D] bg-[#FFD95A] -ml-16 mt-3 absolute w-[28vh] dropdown-content'>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                        </div>
+                    </ul>
+                    <ul className='cursor-pointer hover-dropdown' onClick={dropdown}>
+                        More
+                        <div className='rounded-xl hidden grid-cols-3 justify-around px-2 text-[#4C3D3D] bg-[#FFD95A] -ml-16 mt-3 absolute w-[28vh] dropdown-content'>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                            <li className='text-center'>hello</li>
+                        </div>
+                    </ul>
                 </nav>
             </div>
         </div>
